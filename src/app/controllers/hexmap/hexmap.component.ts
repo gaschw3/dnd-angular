0c05c06deb2e2e9719751ffd0e19f6f4a3f53850
Grant Schwartz <grant.schwartz.08@gmail.com>
2020-08-25 18:02:46 -0400

    maybe foolishly adding a whole set of map-tiles to play with in Leaflet

--- src/app/controllers/hexmap/hexmap.component.ts
+++ src/app/controllers/hexmap/hexmap.component.ts
@@ -1,78 +1,27 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import * as uuid from 'uuid';
-import * as invariant from 'invariant';
-
-import { Component, Input, OnInit, OnDestroy, OnChanges, AfterViewInit } from '@angular/core';
-import { HexGrid, Layout, Hexagon, Text, GridGenerator, HexUtils, Pattern  } from 'react-hexgrid';
-import configs from './configurations.json';
-
-interface HexmapProps {
-    onLoadingChanged?: (any);
-    onError?: (any);
-}
+import { AfterViewInit, Component } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-hexmap',
-  template: '<span></span>',
+  templateUrl: './hexmap.component.html',
   styleUrls: ['./hexmap.component.css']
 })
-export class HexmapComponent implements OnInit, OnDestroy, OnChanges, AfterViewInit {
- 
-    private rootDomID: string;
-     
-    protected getRootDomNode() {
-        const node = document.getElementById(this.rootDomID);
-        invariant(node, `Node '${this.rootDomID} not found!`);
-        return node;
-    }
- 
-    private isMounted(): boolean {
-        return !!this.rootDomID;
-    }
- 
-    protected render() {
-      const config = configs['denari'];
-      const generator = GridGenerator.getGenerator(config.map);
-      const hexagons = generator.apply(this, config.mapProps);
-      var hexes = [];
+export class HexmapComponent implements AfterViewInit {
+  private map;
 
-      hexagons.map((hex, i) => (
-        hexes.push(
-          React.createElement(Hexagon, {"key":config.mapProps+i, "q":hex.q, "r":hex.r, "s":0, "pattern":config.hexType[hex.r + Math.floor(hex.q/2)][hex.q], "className":config.hexType[hex.r + Math.floor(hex.q/2)][hex.q]},
-            React.createElement(Text, null, config.hexType[hex.r + Math.floor(hex.q/2)][hex.q] == "ocean" ? "" : hex.q + "." + ((hex.r+7) + Math.floor(hex.q/2)) )
-          )
-      )));
+  constructor() {  }
 
-      if (this.isMounted()) {
-          ReactDOM.render(
-            React.createElement(HexGrid, { "width":config.width, "height":config.height, "viewBox":"-5 20 100 100"},
-              [
-                React.createElement(Layout, { "size":{"x": config.layout.width, "y": config.layout.height}, "flat":config.layout.flat, "spacing":config.layout.spacing },
-                  [
-                    hexes
-                  ]  
-                ),
-                React.createElement(Pattern, {"key":0, "id":"green", "link":"http://lorempixel.com/400/400/cats/2/", "size":{"x": config.layout.width, "y": config.layout.height}})
-              ]
-            ), this.getRootDomNode()
-          );
-      }
-    }
- 
-    ngOnInit() {
-        this.rootDomID = uuid.v1();
-    }
- 
-    ngOnChanges() {
-        this.render();
-    }
- 
-    ngAfterViewInit() {
-        this.render();
-    }
- 
-    ngOnDestroy() {
-        ReactDOM.unmountComponentAtNode(this.getRootDomNode())
-    }
+  private initMap(): void {
+    var map = L.map('map').setView([30, -120.0], 5.0);
+    L.tileLayer('assets/map-tiles/{z}/{x}/{y}.png', {
+      minZoom: 2,
+      maxZoom: 7,
+      tms: false,
+      attributionControl: false
+    }).addTo(map);
+  }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
 }
