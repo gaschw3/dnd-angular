3b912c8be9f18adcacbf2fc8f4c4981d19f9b160
Grant Schwartz <grant.schwartz.08@gmail.com>
2020-08-31 11:58:13 -0400

    map changes/fixes; adding homebrew magic items

--- src/app/controllers/hexmap/hexmap.component.ts
+++ src/app/controllers/hexmap/hexmap.component.ts
@@ -9,19 +9,144 @@ import * as L from 'leaflet';
 export class HexmapComponent implements AfterViewInit {
   private map;
 
-  constructor() {  }
+  private countries;
+  private continents;
+  private cities;
+  private towns;
+
+  constructor() {
+    this.continents = [
+      ['Ilia', 50, -77],
+    ];
+    this.countries = [
+      ['Denari', 30, -120],
+      ['Florin', 29, -105],
+      ['Pfennig', 19, -105],
+    ];
+    this.cities = [
+      ['Avon', 30, -120.6],
+      ['Noxia', 27.4, -117.9],
+      ['Tarraktur', 32.7, -116.5],
+      ['Arden', 16.57, -100],
+      ['Dalgrange', 22.3, -105.91],
+      ['Tramere', 17.05, -101.18],
+      ['Avonshire', 24.69, -109.4]
+    ]
+    this.towns = [
+      ['Borderland Keep', 30.2, -119.7],
+      ['Norton\'s Point', 30.6, -120.8],
+      ['Ralsburg Ruins', 30, -119.13],
+      ['Elfhome', 29.955, -121.4],
+      ["S. Terminus", 30.29, -118.444],
+      ["N. Terminus", 32.6, -116.95],
+      ["Stag Lord's Fort", 29.47, -120.48 ],
+      ["Sorrow's Furnace", 26.98, -118.416],
+      ["Falurin's Filter", 31.5, -116.565],
+      ["Marv's", 30.5, -120.059],
+      ["Vespid Hive", 30.935, -121.99],
+      ["Volung's Gate", 31.78, -119.267],
+      ["Volung's Gate", 31.51, -120.24],
+      ["Sungaze Monastery", 31.23, -118.92],
+      ["Wordless Word Temple", 29.38, -119.322],
+      ["Druid of Swarms", 29.27, -119.32],
+      ["Mornrise", 21.32, -109.86],
+      ["Penkirk", 22.42, -102.44],
+      ["Trest", 17.93, -104.17],
+      ["Spitemaw", 21.61, -107.02],
+      ["Golden Isle", 21.19, -111.57],
+      ["Bristad", 23.18, -109.32],
+      ["Iverness", 22.34, -109],
+      ["Belhoff", 19.425, -100.57],
+      ["Dale", 19.94, -104.06]
+    ]
+   }
 
   private initMap(): void {
-    var map = L.map('map').setView([30, -120.0], 5.0);
-    L.tileLayer('assets/map-tiles/{z}/{x}/{y}.png', {
+    let map = L.map('map').setView([30, -120.0], 4.0);
+    let textLabel = L.icon({
+      iconUrl: 'my-icon.png',
+      iconSize: [0, 0],
+      iconAnchor: [0, 0],
+      popupAnchor: [0, 0],
+      shadowUrl: 'my-icon-shadow.png',
+      shadowSize: [0, 0],
+      shadowAnchor: [0, 0]
+    });
+    let cityLabel = L.icon({
+      iconUrl: 'assets/city-marker.png',
+      iconSize: [20, 20],
+      iconAnchor: [10, 10],
+      popupAnchor: [0, 25],
+      shadowUrl: 'my-icon-shadow.png',
+      shadowSize: [0, 0],
+      shadowAnchor: [0, 0]
+    });
+    let townLabel = L.icon({
+      iconUrl: 'assets/town-marker.png',
+      iconSize: [20, 20],
+      iconAnchor: [10, 10],
+      popupAnchor: [0, 25],
+      shadowUrl: 'my-icon-shadow.png',
+      shadowSize: [0, 0],
+      shadowAnchor: [0, 0]
+    });
+
+    L.tileLayer('https://raw.githack.com/gaschw3/dnd-map-tiles/master/{z}/{x}/{y}.png', {
       minZoom: 2,
-      maxZoom: 7,
+      maxZoom: 8,
+      maxNativeZoom: 6,
       tms: false,
       attributionControl: false
     }).addTo(map);
+
+    map.on('click', function (e) {
+      var coord = e.latlng;
+      console.log("[" + coord.lat + ", " + coord.lng + "]");
+    });
+
+    for (let i = 0; i < this.continents.length; i++) {
+      let continent = this.continents[i];
+      let marker = new L.marker([continent[1], continent[2]], { opacity: 1, icon: textLabel });
+      marker.bindTooltip(continent[0], { permanent: true, className: "continent-label", direction: "center", offset: [0, 0] });
+      marker.addTo(map);
+    }
+    for (let i = 0; i < this.countries.length; i++) {
+      let country = this.countries[i];
+      let marker = new L.marker([country[1], country[2]], { opacity: 1, icon: textLabel });
+      marker.bindTooltip(country[0], { permanent: true, className: "country-label", direction: "center", offset: [0, 0] });
+      marker.addTo(map);
+    }
+    for (let i = 0; i < this.cities.length; i++) {
+      let city = this.cities[i];
+      let marker = new L.marker([city[1], city[2]], { opacity: 0, icon: cityLabel, className: "cityLabel" });
+      marker.bindTooltip(city[0], { permanent: true, className: "city-label", direction: "center", opacity: 0, offset: [0, 20] });
+      marker.addTo(map);
+    }
+    for (let i = 0; i < this.towns.length; i++) {
+      let town = this.towns[i];
+      let marker = new L.marker([town[1], town[2]], { opacity: 0, icon: townLabel, className: "townLabel" });
+      marker.bindTooltip(town[0], { permanent: true, className: "town-label", direction: "center", opacity: 0, offset: [0, 10] });
+      marker.addTo(map);
+    }
+
+    this.changeZoom(map, ".country-label", 3, 5);
+    this.changeZoom(map, ".city-label", 5, 8);
+    this.changeZoom(map, ".town-label", 7, 8);
+    this.changeZoom(map, ".leaflet-marker-icon", 6, 8);
   }
 
   ngAfterViewInit(): void {
     this.initMap();
+  }
+
+  changeZoom(map, targetLayer, zoomMin, zoomMax) {
+    map.on('zoom', function () {
+      let zoom = map.getZoom();
+      if (zoom > zoomMax || zoom < zoomMin) {
+        $(targetLayer).fadeTo(1000, 0, function () {  });
+      } else if (zoom <= zoomMax && zoom >= zoomMin) {
+        $(targetLayer).fadeTo(1000, 1, function () { });
+      }
+    });
   }
 }
