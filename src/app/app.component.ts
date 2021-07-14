de63dc17ebfe8c5cb41b568c48ee78582a3a17f5
Grant Schwartz <grant.schwartz@lexmark.com>
2021-07-14 12:56:25 -0400

    add theme select and move into actual component, general styling fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dnd-angular';
+  defaultTheme = "dark";
+  theme= this.getCookie("theme");
+
+  setCookie (name, value, days) {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days*24*60*60*1000));
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+  }
+
+  getCookie (name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return this.defaultTheme; //use dark mode if no cookie is set
+  }
+
+  changeTheme() {
+    this.setCookie("theme", this.theme,30); //set "user_email" cookie, expires in 30 days
+  }
+  
 }
--- src/app/controllers/particles/particles.component.ts
+++ src/app/controllers/particles/particles.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-particles',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParticlesComponent implements OnInit {
 
+  @Input() theme: string;
   constructor() { }
 
   ngOnInit(): void {
